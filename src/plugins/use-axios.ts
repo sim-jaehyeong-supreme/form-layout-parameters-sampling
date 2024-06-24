03d96c1600ada84f0029fa590a45a1180bfd407c
sim-jaehyeong-supreme <sim_jaehyeong@supreme-system.com>
2024-06-24 13:59:15 +0900

    stackblitz deploy test

--- src/plugins/use-axios.ts
+++ src/plugins/use-axios.ts
@@ -2,7 +2,7 @@ import axiosOriginal from 'axios';
 
 export const useAxios = () => {
     const axios = axiosOriginal.create({
-        baseURL: "http://localhost:7777",
+        baseURL: "http://localhost:3000",
         headers: {
             'Content-Type': 'application/json',
           },
